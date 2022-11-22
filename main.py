9cd005a4250903a2d0388b33cfcebc4a781490c1
ZaNoZa2405 <senior.slender2015@gmail.com>
2022-11-22 13:54:16 +0200

    Code to run in the console

---
+++ main.py
@@ -0,0 +1,17 @@
+# import module
+from gnewsclient import gnewsclient
+
+# declare a NewsClient object
+client = gnewsclient.NewsClient(language='ukrainian',  # print("Location: \n", client.locations, '\n')
+                                location='Ukraine', #print("Language \n", client.languages, '\n')
+                                topic='World', #print("Topic \n", client.topics, '\n')
+                                max_results=10)  # Your number of requests
+
+news_list = client.get_news()
+
+for item in list(news_list):
+    print("\nTitle : ", item['title'])
+    print("Link : ", item['link'], '\n')
+
+
+
